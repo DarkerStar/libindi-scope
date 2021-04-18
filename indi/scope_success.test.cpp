2f220b27cd9e0ffc54bd417bef63c557aced832a
Mark A. Gibbs <indi.in.the.wired@gmail.com>
2021-04-18 11:08:59 -0400

    Test that scope_success is not copy constructible

--- indi/scope_success.test.cpp
+++ indi/scope_success.test.cpp
@@ -115,3 +115,9 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(not_default_constructible, Func, indi_test::all_fu
 	BOOST_TEST(not std::is_default_constructible_v<indi::scope_exit<Func>>);
 	BOOST_TEST(not std::is_default_constructible_v<indi::scope_exit<Func&>>);
 }
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(not_copy_constructible, Func, indi_test::all_functors<int>)
+{
+	BOOST_TEST(not std::is_copy_constructible_v<indi::scope_exit<Func>>);
+	BOOST_TEST(not std::is_copy_constructible_v<indi::scope_exit<Func&>>);
+}
